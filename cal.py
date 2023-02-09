8052eb3420a56a5effbcdfd716ab7d957cd13469
JigarSoni1006 <jigarsoni1006@gmail.com>
2023-02-09 22:59:27 +0530

    add new file

---
+++ cal.py
@@ -0,0 +1,14 @@
+
+
+a=input("Enter Value 1- ")
+print ("Value 1- ",a)
+print (type(a))
+x = int(a)
+print(x)
+print(type(x))
+b=input("Enter Value 2- ")
+print("value 2- ",b)
+y=int(b)
+total = x+y
+print("Total >>>>",total)
+
